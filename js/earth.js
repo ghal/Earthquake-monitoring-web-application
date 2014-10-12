07d05bd56937df93be73ab3d2fa6bc5115d5e1a6
Giannis Halvatzakis <ghalvatzakis2@gmail.com>
2014-10-12 19:24:36 +0300

    Added geolocation service using ip and/or geolocation API

--- js/earth.js
+++ js/earth.js
@@ -1,22 +1,36 @@
 // The following example creates complex markers to indicate earthquakes all over the world.
-$( document ).ready(function() {
-
-
-
+var geocoder;
+var map;
 function initialize() {
-var mapOptions = {
-         zoom: 7,
+  geocoder = new google.maps.Geocoder();
+  var mapOptions = {
+         zoom: 3,
          center: new google.maps.LatLng(38.092913, 24.9346141),
          disableDefaultUI: true
   }
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
+   map = new google.maps.Map(document.getElementById('map-canvas'),
                                 mapOptions);
-
-
+  codeAddress();
   setMarkers(map, beaches);
 
 }
 
+function codeAddress() {
+  var address = userLocation;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      console.log(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
 /**
  * Data for the markers consisting of a name, a LatLng and a zIndex for
  * the order in which these markers should display on top of each
@@ -72,9 +86,43 @@ function setMarkers(map, locations) {
     });
   }
 }
+var x = document.getElementById("demo");
 
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition,showError);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    var latlon = position.coords.latitude+","+position.coords.longitude;
+
+    var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="
+    +latlon+"&zoom=1&size=400x300&sensor=false";
+    document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+}
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred."
+            break;
+    }
+}
+
+getLocation();
 
 
 
 google.maps.event.addDomListener(window, 'load', initialize);
-});
--- js/locations.js
+++ js/locations.js
@@ -1,4 +1,5 @@
 var beaches = [];
+var userLocation;
 $.getJSON('test.json', function(data) {
     for (var ii=0;ii<=10;ii++){
       var lan = data.features[ii].geometry.coordinates[1];
@@ -6,3 +7,7 @@ $.getJSON('test.json', function(data) {
       beaches.push([data.features[ii].properties.place, lan,lon, 2]);
      }
 });
+$.get("http://ipinfo.io", function(response) {
+    userLocation = response.country;
+    console.log(response.city, response.region, response.country);
+}, "jsonp");
