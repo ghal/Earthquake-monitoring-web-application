4941a660f0cfb58dfd71e0eb49d3883b7af2b6eb
Giannis Halvatzakis <ghalvatzakis2@gmail.com>
2014-10-13 14:19:17 +0300

    Replaced markers with Circles

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ app.controller('MainController', ['$http', function($http){
 	var seismoi = this;
 	seismoi.test =[];
 	//document.write('<h1>LOADING...</h1>');
-	$http.get('test.json').success(function(data){
+	$http.get('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson').success(function(data){
 		seismoi.test = data;
 	});
 
--- js/locations.js
+++ js/locations.js
@@ -1,12 +1,24 @@
 var beaches = [];
 var userLocation;
-$.getJSON('test.json', function(data) {
-    for (var ii=0;ii<=10;ii++){
-      var lan = data.features[ii].geometry.coordinates[1];
-      var lon = data.features[ii].geometry.coordinates[0];
-      beaches.push([data.features[ii].properties.place, lan,lon, 2]);
-     }
-});
+
+// var jsonUrl = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson?callback=?";
+// $.getJSON(jsonUrl, function(data) {
+//     for (var ii=0;ii<=10;ii++){
+//       var lan = data.features[ii].geometry.coordinates[1];
+//       var lon = data.features[ii].geometry.coordinates[0];
+//       beaches.push([data.features[ii].properties.place, lan,lon, data.features[ii].properties.mag]);
+//     }
+// });
+
+$.get("test.json", function(data) {
+        for (var ii=0;ii<=100;ii++){
+          var lan = data.features[ii].geometry.coordinates[1];
+          var lon = data.features[ii].geometry.coordinates[0];
+          beaches.push([data.features[ii].properties.place, lan,lon, data.features[ii].properties.mag]);
+          //console.log(data);
+        }
+}, "json");
+
 $.get("http://ipinfo.io", function(response) {
     userLocation = response.country;
     console.log(response.city, response.region, response.country);
